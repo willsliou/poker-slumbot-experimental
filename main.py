63e4c75fc9291b4d12d12bb986ab526c1a71e05d
Wills Liou <willsliou@protonmail.com>
2021-12-31 22:53:28 +0000

    Added rollingBetSize to track rolling sum of all bets to be added at end of pot. Working version without fold implementation

--- main.py
+++ main.py
@@ -14,17 +14,11 @@ class Dealer:
   def reset(self):
     self.deck = []
     suits = ['h', 'd', 'c', 's']
-    ranks = ['A', 2,3,4,5,6,7,8,9,10, 'J', 'Q', 'K']
+    ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 
     for rank in ranks:
       for suit in suits:
         self.deck.append(str(rank) + suit)
-
-  # Deal next card
-
-  # def dealCards1(self):
-  #   Player.hand.append(self.deck.pop())
-  #   Player.hand.append(self.deck.pop())
     
   def deal(self):
     return self.deck.pop()
@@ -39,18 +33,22 @@ class Player:
     self.chips = chips
     # self.hand = [None] * 5 # 5 total cards
     self.hand = []
+    self.betSize = 0
 
   def fold(self):
-    print(currPlayer.name, "folds.")
+    print(self.name, "folds.")
 
   def bet(self, betSize):
     # GameState.betSize = betSize
     self.chips = self.chips - betSize
-    print(self.name, " bets $", betSize, "Chips left $", self.chips)
+    self.betSize = betSize
+    print(self.name, " bets $", betSize, "Total chips: $", self.chips)
 
   def check(self):
-    print(currPlayer.name, 'checks.')
-  
+    print(self.name, 'checks.', "Total chips: $", self.chips)
+
+  def call(self):
+    print(self.name, "calls. Total chips: $", self.chips)
 
 
 class Card:
@@ -66,13 +64,26 @@ class Card:
 
     
 class GameState:
-    def __init__(self, currAction, betSize, remainingPlayerTurns):
+    def __init__(self, currAction, betSize, rollingBetSize, remainingPlayerTurns):
+      """
+      betSize = current bet on the table
+      rollingBetSize = total of all bets during a single turn
+      remainingPlayerTurns = Tracks remaining number of turns after a player has bet
+      """
       self.currAction = currAction
       self.pot = 0
       self.betSize = betSize
+      self.rollingBetSize = rollingBetSize
       self.remainingPlayerTurns = remainingPlayerTurns
-    # def setBet(size):
-      
+
+    """
+    Set a bet. 
+    Current bet to be matched is betSize.
+    rollingBetSize is the total of all bets. Increase rollingBetSize by value of new bet.
+    """
+    def setBet(self, betSize_):
+      g.betSize = betSize_
+      g.rollingBetSize += betSize_
       
     
 #   def playGame():
@@ -89,7 +100,7 @@ d.shuffle()
 # Deal cards to players
 players = [] # Player array to store all total players
 # numPlayers = int(input("Enter total number of players: "))
-numPlayers = 3
+numPlayers = 6
 # startingChips = int(input("Enter starting chip size: "))
 startingChips = 1000
 
@@ -103,30 +114,25 @@ for i in range(numPlayers):
 
 
   # def startGame():
-g = GameState(0, 0, numPlayers)
-# p1 = Player('Joe Biden', 1000)
-# p2 = Player('Trump', 1000)
-# players.append(p1)
-# players.append(p2)
+g = GameState(0, 0, 0, numPlayers)
 
-# p1.hand.append(d.deal())
-# p1.hand.append(d.deal())
-# p2.hand.append(d.deal())
-# p2.hand.append(d.deal())
-# print("Player 1: ", p1.hand)
-# print("Player 2: ", p2.hand)
+"""
+Simulates full round with 3 turns. (Preflop, Flop, Turn, River)
+For three rounds, if there are remaining turns, take each player's action.
+If player calls, subtract current player's chipsize with bet size.
 
-# action = input("Choose an action. Check: 'k', Call: 'c', Bet: 'b', Fold: 'f' ")
-
-# Simulate full round with 3 turns. (Preflop, Flop, Turn, River)
+"""
 currPlayer_i = 0
 totalTurns = 0
+# 3 total rounds
 for i in range(0, 3):
   # j represents the current player, totalTurns represents total turns
   g.remainingPlayerTurns = numPlayers
   while (g.remainingPlayerTurns > 0):
     currPlayer = players[currPlayer_i]
     print(currPlayer.name, "'s turn'")
+    
+    # Ask for a valid action
     # If someone raised, change actions to Call, Raise, or Fold
     if g.betSize > 0:
       action = input("Choose an action. Call: 'c', Raise: 'b', Fold: 'f'")
@@ -134,14 +140,16 @@ for i in range(0, 3):
     else:
       action = input("Choose an action. Check: 'k', Bet: 'b', Fold: 'f' ")
 
-      
+    ################################
+    ######## Player Actions ########
+    ################################
     # Player calls
     if action == 'c':
       # If current player calls, subtract players chips
       currPlayer.chips -= g.betSize
-      # Double individual player's bets. Add bet size to total pot.
-      g.pot += (g.betSize*2)
-      print(currPlayer.name, "calls. Total chips: $", currPlayer.chips)
+      # Add new bet to total betSize
+      g.rollingBetSize += g.betSize
+      currPlayer.call()
       
     # Player bets, ask for input. Call Player.bet() to reduce individual player chip size. Increase global bet size.
     elif action == 'b':
@@ -153,31 +161,37 @@ for i in range(0, 3):
       # if betSize_
       # Shove all in
       currPlayer.bet(betSize_)
+      g.setBet(betSize_)
       g.remainingPlayerTurns = numPlayers
-      g.betSize += betSize_
-      print("Global betsize:", g.betSize)
+      print("Current bet to be matched:", g.betSize)
     
     # Player checks, continue to next round
     elif action == 'k':
       currPlayer.check()
-      # print("Player checks")
   
     # Player folds, muck cards
     elif action == 'f':
       currPlayer.fold()
-      raise StopIteration
+      continue
 
     g.remainingPlayerTurns -= 1
     totalTurns += 1
-    print("totalTurns: ", totalTurns)
+    # print("totalTurns: ", totalTurns)
     currPlayer_i = totalTurns % len(players) # Cycle between players using mod
-    print("currPlayer_i: ", currPlayer_i)
+    # print("currPlayer_i: ", currPlayer_i)
     print("g.remainingPlayerTurns", g.remainingPlayerTurns)
     print("g.betSize: ", g.betSize)
+    print("g.rollingBetSize: ", g.rollingBetSize)
 
+  # After all players have taken their turns.
   if g.remainingPlayerTurns == 0:
+    # Add all bets into pot
+    g.pot += g.rollingBetSize
     # After everyone calls, global bet size == 0
     g.betSize = 0
+    # Reset rollingSum of all bets
+    g.rollingBetSize = 0
+    print("g.pot at end of all turns: ", g.pot)
   # Flop turns three cards.
   if g.remainingPlayerTurns == 0 and turnCounter == 0:
     d.community_cards.append(d.deal())
