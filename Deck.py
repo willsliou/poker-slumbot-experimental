e440a6a312b5de2aa9586dd3ddf5dacce39d9f40
Wills Liou <willsliou@protonmail.com>
2022-01-02 10:47:59 -0800

    frequency class

--- Deck.py
+++ Deck.py
@@ -1,6 +1,9 @@
 import random as random
 
 class Card:
+  """
+  Represents a normal playing card
+  """
   # Class attributes  
   suits = ['h', 'd', 'c', 's']
   ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
@@ -16,7 +19,7 @@ class Card:
 # Inherits from Card
 class Deck(Card):
   """
-  Represents a deck of cards
+  Represents a deck of normal playing cards
   """
   def __init__(self):
     # self.deck = [None] * 52 # Initialize fixed array size
@@ -64,3 +67,31 @@ class Deck(Card):
     ans = [str(card) for card in self.deck_of_cards]
     return '\n'.join(ans)
 
+
+# Hand inherits from Deck
+class Hand(Deck):
+  """
+  Represents a player's hand of cards
+  """
+  def __init__(self, name = ''):
+    self.name = name
+    self.deck_of_cards = []
+    self.numPlayers = 6
+
+  def add_card(self, card):
+    self.deck_of_cards.append(card)
+
+  def __str__(self): 
+      return "Hand is % s, ""b is % s" % (self.deck_of_cards, self.name)
+
+class Frequency(dict):
+  def __init__(self, arr = []):
+    for i in arr:
+      self.count()
+
+  def count(self, item, incr):
+    self[item] = self.get(item, 0) + incr
+
+    if self[item] == 0:
+      del self[item]
+    
