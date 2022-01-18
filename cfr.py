751bcdcad78dc9e268011de3c3614c305ca520d8
Wills Liou <willsliou@protonmail.com>
2022-01-18 22:09:53 +0000

    test propogation

--- cfr.py
+++ cfr.py
@@ -12,17 +12,15 @@ class CounterfactualRegretMinimizer:
   def __init__(self):
     self.v1 = []
     self.v2 = []
-    self.v3 = []
+    self.left_utility = []
 
-def propogate_utility(self, state):
-  if state.isEnd():
-    return state.eval()
-    
-
-def utility(self, state):
-  if state.isEnd():
-    return 0
-
-  ans = 0
-  for a in state.actions:
-    left_node = self.
+  def propogate_utility(self, state):
+    if state.isEnd():
+      return state.eval()
+      
+    ans = 0
+    for a in state.actions:
+      self.propogate_utility(state.play(a))
+      ans += self.sig[state.mySet()][a] * self.left_utility
+    return ans
+  
